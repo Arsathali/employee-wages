5aae73c7aa4356aada69996ed74e68d5b4db6668
arsath ali <arsatharsha17@gmail.com>
2026-03-28 15:53:28 +0530

    Added UC 6: Wages till Total Hours or Days Reached

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 /* 
-   UC 5: Calculating Wages for a Month
+   UC 6: Wages till Total Hours or Days Reached
 */
 function getEmployeeHours() {
   let empCheck = Math.floor(Math.random() * 3); // 0,1,2
@@ -11,16 +11,23 @@ function getEmployeeHours() {
   }
 }
 
+let totalHours = 0;
+let day = 0;
+let MAX_TOTAL_HOURS = 100;
+let MAX_TOTAL_DAYS = 20;
 let dailyWages = [];
 
-for(let i=0; i<5; i++){  // simulate 5 days
+while(totalHours < MAX_TOTAL_HOURS && day < MAX_TOTAL_DAYS){  // simulate 5 days
+    day++;
     let wagePerHour = 20;
     let hours = getEmployeeHours();
+    totalHours += hours;
     let wage = hours * wagePerHour;
     dailyWages.push(wage);
 }
 
 let totalWages = dailyWages.reduce((acc,curr) => acc+curr, 0);
 
-console.log("Daily Wage:", dailyWages);
-console.log(totalWages);
+console.log("Daily Wage: ", dailyWages);
+console.log("Total Wages: ",totalWages);
+console.log("Toatl Hours: ",totalHours);
