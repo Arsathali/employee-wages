860395f8d02c270d7369494217911d558c3fca06
arsath ali <arsatharsha17@gmail.com>
2026-03-28 15:58:57 +0530

    Added UC 7: Refactor to Class Method

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 
 /* 
-   UC 6: Wages till Total Hours or Days Reached
+   UC 7: Refactor to Class Method
 */
+class EmployeeWage {
+
+  constructor(company, wagePerHour, maxDays, maxHours) {
+    this.company = company;
+    this.wagePerHour = wagePerHour;
+    this.maxDays = maxDays;
+    this.maxHours = maxHours;
+    this.dailyWage = [];
+    this.totalWage = 0;
+  }
+
+  computeWage() {
+
+    let totalHours = 0;
+    let day = 0;
+
+    while(day < this.maxDays && totalHours < this.maxHours) {
+
+      let hours = getEmployeeHours();
+      let wage = hours * this.wagePerHour;
+      this.dailyWage.push(wage);
+      this.totalWage += wage;
+      totalHours += hours;
+      day++;
+
+    }
+  }
+
+  printWage() {
+    console.log(`Company: ${this.company}`);
+    console.log("Daily Wages:", this.dailyWage);
+    console.log("Total Wage:", this.totalWage);
+  }
+}
+
 function getEmployeeHours() {
   let empCheck = Math.floor(Math.random() * 3); // 0,1,2
   switch(empCheck) {
@@ -11,23 +46,6 @@ function getEmployeeHours() {
   }
 }
 
-let totalHours = 0;
-let day = 0;
-let MAX_TOTAL_HOURS = 100;
-let MAX_TOTAL_DAYS = 20;
-let dailyWages = [];
-
-while(totalHours < MAX_TOTAL_HOURS && day < MAX_TOTAL_DAYS){  // simulate 5 days
-    day++;
-    let wagePerHour = 20;
-    let hours = getEmployeeHours();
-    totalHours += hours;
-    let wage = hours * wagePerHour;
-    dailyWages.push(wage);
-}
-
-let totalWages = dailyWages.reduce((acc,curr) => acc+curr, 0);
-
-console.log("Daily Wage: ", dailyWages);
-console.log("Total Wages: ",totalWages);
-console.log("Toatl Hours: ",totalHours);
+let emp = new EmployeeWage("DMart", 20, 20, 100);
+emp.computeWage();
+emp.printWage();
